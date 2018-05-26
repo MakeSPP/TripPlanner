7e715c586e4ea0372a24fe3a0293823512b53006
TJ <makertech81@gmail.com>
2018-05-26 13:23:17 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+		function changeSelect() {
+    if (document.getElementById("select1").value == "1") {
+        document.getElementById("info1").innerHTML = "Vegas Info";
+    } else if (document.getElementById("select1").value == "2") {
+        document.getElementById("info1").innerHTML = "chicago";
+    } else if (document.getElementById("select1").value == "3") {
+        document.getElementById("info1").innerHTML = "la";
+    } else if (document.getElementById("select1").value == "4") {
+        document.getElementById("info1").innerHTML = "nyc";
+    }
+    else if (document.getElementById("select1").value == "5") {
+        document.getElementById("info1").innerHTML = "tokyo";
+    } else if (document.getElementById("select1").value == "6") {
+        document.getElementById("info1").innerHTML = "London";
+    }
+}
+function changeSelect2() {
+    if (document.getElementById("select2").value == "1") {
+        document.getElementById("info2").innerHTML = "Vegas Info";
+    } else if (document.getElementById("select2").value == "2") {
+        document.getElementById("info2").innerHTML = "chicago";
+    } else if (document.getElementById("select2").value == "3") {
+        document.getElementById("info2").innerHTML = "la";
+    } else if (document.getElementById("select2").value == "4") {
+        document.getElementById("info2").innerHTML = "nyc";
+    }
+    else if (document.getElementById("select2").value == "5") {
+        document.getElementById("info2").innerHTML = "tokyo";
+    } else if (document.getElementById("select2").value == "6") {
+        document.getElementById("info2").innerHTML = "London";
+    }
+}
