fbd8e6b084a90f4d697ea62ce3b6eeff3b57b575
TJ <makertech81@gmail.com>
2018-05-26 16:52:19 -0400

    Add files via upload

--- js/makespp.js
+++ js/makespp.js
@@ -1,32 +1,38 @@
-		function changeSelect() {
+function changeSelect() {
     if (document.getElementById("select1").value == "1") {
-        document.getElementById("info1").innerHTML = "Vegas Info";
+        document.getElementById("info1").innerHTML = "Vegas Info <br><font size=4>Estimate Population (2017): 648,224 <br> Founded: 1905 <br> Current Mayor: Carolyn Goodman<br> Fun Fact: With its millions of lights, Las Vegas is considered the brightest spot on Earth.";
     } else if (document.getElementById("select1").value == "2") {
-        document.getElementById("info1").innerHTML = "chicago";
+        document.getElementById("info1").innerHTML = "Venice Info<br><font size=4>Estimate Population (2014): 264,557 <br> Founded: 697 AD <br> Current Mayor: Luigi Brugnaro <br>Fun Fact: Venice is made up of a collection of approx 115 tiny islands";
     } else if (document.getElementById("select1").value == "3") {
-        document.getElementById("info1").innerHTML = "la";
+        document.getElementById("info1").innerHTML = "San Francisco Info<br><font size=4>Estimate Population (2017): 884,363<br>Founded: 1776<br>Named for: St. Francis of Assisi<br> Current Mayor: Mark Farrell<br>Fun Fact: San Francisco has more dogs than children";
     } else if (document.getElementById("select1").value == "4") {
-        document.getElementById("info1").innerHTML = "nyc";
+        document.getElementById("info1").innerHTML = "New York City Info<br><font size=4>Estimate Population (2017): 8,622,698<br>Setted: 1624<br>Named for: James, Duke of York<br>Current Mayor: Bill de Blasio<br>Fun Fact: New York was the first state to require license plates on cars.";
     }
     else if (document.getElementById("select1").value == "5") {
-        document.getElementById("info1").innerHTML = "tokyo";
+        document.getElementById("info1").innerHTML = "Tokyo Info<br><font size=4>Estimate Population (2016): 13, 617,444<br>Founded: 1457<br>Current Governor: Yuriko Koike<br>Fun Fact: Tokyo's railway stations can be so busy that some have employees called 'pushers' that shove people onto the trains when it is busy.";
     } else if (document.getElementById("select1").value == "6") {
-        document.getElementById("info1").innerHTML = "London";
+        document.getElementById("info1").innerHTML = "London Info<br><font size=4>Estimate Population (2016): 8,787,892<br>Founded: 43 AD<br>Current Mayor: Sadiq Khan<br>Fun Fact: New York Police Department also operates in London.";
     }
-}
+    else if (document.getElementById("select1").value == "7") {
+        document.getElementById("info1").innerHTML = "Paris Info<br><font size=4>Estimate Population (2018): 2,206,488<br>Founded: 52 BC<br>Current: Mayor: Anne Hidalgo<br>Fun Fact: There’s only one stop sign in Paris.";
+    }
+  }
 function changeSelect2() {
     if (document.getElementById("select2").value == "1") {
-        document.getElementById("info2").innerHTML = "Vegas Info";
+        document.getElementById("info2").innerHTML = "Vegas Info <br><font size=4>Estimate Population (2017): 648,224 <br> Founded: 1905 <br> Current Mayor: Carolyn Goodman<br> Fun Fact: With its millions of lights, Las Vegas is considered the brightest spot on Earth.";
     } else if (document.getElementById("select2").value == "2") {
-        document.getElementById("info2").innerHTML = "chicago";
+        document.getElementById("info2").innerHTML = "Venice Info<br><font size=4>Estimate Population (2014): 264,557 <br> Founded: 697 AD <br> Current Mayor: Luigi Brugnaro <br>Fun Fact: Venice is made up of a collection of approx 115 tiny islands";
     } else if (document.getElementById("select2").value == "3") {
-        document.getElementById("info2").innerHTML = "la";
+        document.getElementById("info2").innerHTML = "San Francisco Info<br><font size=4>Estimate Population (2017): 884,363<br>Founded: 1776<br>Named for: St. Francis of Assisi<br> Current Mayor: Mark Farrell<br>Fun Fact: San Francisco has more dogs than children";
     } else if (document.getElementById("select2").value == "4") {
-        document.getElementById("info2").innerHTML = "nyc";
+        document.getElementById("info2").innerHTML = "New York City Info<br><font size=4>Estimate Population (2017): 8,622,698<br>Setted: 1624<br>Named for: James, Duke of York<br>Current Mayor: Bill de Blasio<br>Fun Fact: New York was the first state to require license plates on cars.";
     }
     else if (document.getElementById("select2").value == "5") {
-        document.getElementById("info2").innerHTML = "tokyo";
+        document.getElementById("info2").innerHTML = "Tokyo Info<br><font size=4>Estimate Population (2016): 13, 617,444<br>Founded: 1457<br>Current Governor: Yuriko Koike<br>Fun Fact: Tokyo's railway stations can be so busy that some have employees called 'pushers' that shove people onto the trains when it is busy.";
     } else if (document.getElementById("select2").value == "6") {
-        document.getElementById("info2").innerHTML = "London";
+        document.getElementById("info2").innerHTML = "London Info<br><font size=4>Estimate Population (2016): 8,787,892<br>Founded: 43 AD<br>Current Mayor: Sadiq Khan<br>Fun Fact: New York Police Department also operates in London.";
     }
-}
+    else if (document.getElementById("select2").value == "7") {
+        document.getElementById("info2").innerHTML = "Paris Info<br><font size=4>Estimate Population (2018): 2,206,488<br>Founded: 52 BC<br>Current: Mayor: Anne Hidalgo<br>Fun Fact: There’s only one stop sign in Paris.";
+    }
+  }
